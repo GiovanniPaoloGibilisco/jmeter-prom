2022bafd93a2c34f9744a2fccc6b4d6315e3e069
mvuser <mvuser@mvmb114.lab.moviri.com>
2017-06-28 12:32:58 +0200

    Initial commit

---
+++ client/run_test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Usage ./run_test.sh HOST_IP SCRIPT_NAME REMOTE_SERVERS
+#Example: ./run_test.sh 192.168.100.114 moviri_load_test.jmx "192.168.100.117,192.168.100.118"
+docker run --rm --net=host -e HOST_IP="$1" -e SCRIPT_NAME="$2" -e REMOTE_SERVERS="$3" -v /home/mvuser/MAS/images/jmeter/client/data:/tmp/data --name jmeter-client giovannipaologibilisco/jmeter:client
+
