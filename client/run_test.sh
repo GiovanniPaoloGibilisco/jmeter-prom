c4fa84a7420cfc43d6c68bb2eb007a9fc1e9f869
mvuser <mvuser@mvmb114.lab.moviri.com>
2017-06-29 15:55:14 +0200

    add url variable as an environment variable

--- client/run_test.sh
+++ client/run_test.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-#Usage ./run_test.sh HOST_IP SCRIPT_NAME REMOTE_SERVERS
-#Example: ./run_test.sh 192.168.100.114 moviri_load_test.jmx "192.168.100.117,192.168.100.118"
-docker run --rm --net=host -e HOST_IP="$1" -e SCRIPT_NAME="$2" -e REMOTE_SERVERS="$3" -v /home/mvuser/git/jmeter-prom/client/data:/tmp/data --name jmeter-client giovannipaologibilisco/jmeter-prom:client
+#Usage ./run_test.sh HOST_IP SCRIPT_NAME REMOTE_SERVERS SERVER_IP
+# HOST_IP = jmeter master ip (used for rmi from injectors)
+# SCRIPT_NAME = name of thescript to run (must be in the data directory)
+# REMOTE_SERVERS = list of injector ips
+# SERVER_IP = the ip of the server hosting the application (this is mapped to the "url" variable inside the jmeter script)
+#Example: ./run_test.sh 192.168.100.114 moviri_load_test.jmx "192.168.100.117,192.168.100.118" 192.168.100.116
+docker run --rm --net=host -e HOST_IP="$1" -e SCRIPT_NAME="$2" -e SERVER_IP="$4" -e REMOTE_SERVERS="$3" -v /home/mvuser/git/jmeter-prom/client/data:/tmp/data --name jmeter-client giovannipaologibilisco/jmeter-prom:client
